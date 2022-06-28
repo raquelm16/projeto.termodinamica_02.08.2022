b23db6acc63dee26a1a97c0a59cc7404383bd41b
Raquel <100868145+raquelm16@users.noreply.github.com>
2022-06-28 20:36:14 -0300

    Add files via upload

---
+++ js/eficiencia.js
@@ -0,0 +1,20 @@
+let limp = document.querySelector('#limp')
+limp.addEventListener('click', Limpar)
+let ef = document.querySelector('#ef')
+ef.addEventListener('click', eficienciaCalc)
+
+function Limpar(){
+  t1 = (" ")
+  q1 = (" ")
+  res1 = (" ")
+  document.getElementById('t').value = t1
+  document.getElementById('q').value = q1
+  document.getElementById('res').value = res1
+}
+      
+function eficienciaCalc(){
+  let t = Number.parseFloat(document.getElementById('t').value) 
+  let q = Number.parseFloat(document.getElementById('q').value)
+  let res = t / q
+  document.getElementById('res').value = res
+}
